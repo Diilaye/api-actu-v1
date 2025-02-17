ae37ebceec6add44e90bc7980d667100f7c20e46
Issa laye KANE <issa.k@deally.fr>
2025-02-17 23:05:59 +0000

    add news version

--- routes/posts-digiteaux.js
+++ routes/posts-digiteaux.js
@@ -4,9 +4,9 @@ const router = new require('express').Router();
 router.get('/', require('../controllers/posts-digiteaux').all);
 
 
-router.post('/', require('../middleweares/auth').checkRole('administrateur'), require('../controllers/posts-digiteaux').add);
+router.post('/', require('../middleweares/auth').checkManyRole(['administrateur' ,'redacteur']), require('../controllers/posts-digiteaux').add);
 
-router.put('/:id', require('../middleweares/auth').checkRole('administrateur'), require('../controllers/posts-digiteaux').update);
+router.put('/:id', require('../middleweares/auth').checkManyRole(['administrateur' ,'redacteur']), require('../controllers/posts-digiteaux').update);
 
 
 
