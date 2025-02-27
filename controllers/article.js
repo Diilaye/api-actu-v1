4ff7728ef46478e1dc00754e4b1358c9434850e5
Issa laye KANE <issa.k@deally.fr>
2025-02-27 02:03:20 +0000

    add newdecro

--- controllers/article.js
+++ controllers/article.js
@@ -271,6 +271,333 @@ exports.uneArticles = async (req, res) => {
     }
 };
 
+exports.articleActualite = async (req, res) => {
+    try {
+        const articles = await articleModel.find({categorie: "669b0b3bfe714afd6cf57389"  , typeUne: { $not: /rubrique/ }})
+            .sort({ date: -1 })  // Trier par date (du plus récent au plus ancien)
+            .limit(10)                  // Garder uniquement les 5 derniers
+            .populate(objectPopulate)
+            .exec();
+
+        return res.status(200).json({
+            message: 'Liste des 5 derniers articles récupérée avec succès',
+            status: 'OK',
+            data: articles,
+            statusCode: 200
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Erreur serveur',
+            status: 'NOT OK',
+            error: error.message,
+            statusCode: 500
+        });
+    }
+};
+
+exports.articlePolitique = async (req, res) => {
+    try {
+        const articles = await articleModel.find({categorie: "669b0b67fe714afd6cf5738b"  , typeUne: { $not: /rubrique/ }})
+            .sort({ date: -1 })  // Trier par date (du plus récent au plus ancien)
+            .limit(5)                  // Garder uniquement les 5 derniers
+            .populate(objectPopulate)
+            .exec();
+
+        return res.status(200).json({
+            message: 'Liste des 5 derniers articles récupérée avec succès',
+            status: 'OK',
+            data: articles,
+            statusCode: 200
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Erreur serveur',
+            status: 'NOT OK',
+            error: error.message,
+            statusCode: 500
+        });
+    }
+};
+
+exports.unePolitique = async (req, res) => {
+    try {
+        const articles = await articleModel.find({categorie: "669b0b67fe714afd6cf5738b"  , typeUne:  "rubrique" })
+            .sort({ date: -1 })  // Trier par date (du plus récent au plus ancien)
+            .limit(1)                  // Garder uniquement les 5 derniers
+            .populate(objectPopulate)
+            .exec();
+
+        return res.status(200).json({
+            message: 'Liste des 5 derniers articles récupérée avec succès',
+            status: 'OK',
+            data: articles[0],
+            statusCode: 200
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Erreur serveur',
+            status: 'NOT OK',
+            error: error.message,
+            statusCode: 500
+        });
+    }
+};
+
+exports.articleEconomie = async (req, res) => {
+    try {
+        const articles = await articleModel.find({categorie: "669b0b7cfe714afd6cf5738d"  , typeUne: { $not: /rubrique/ }})
+            .sort({ date: -1 })  // Trier par date (du plus récent au plus ancien)
+            .limit(5)                  // Garder uniquement les 5 derniers
+            .populate(objectPopulate)
+            .exec();
+
+        return res.status(200).json({
+            message: 'Liste des 5 derniers articles récupérée avec succès',
+            status: 'OK',
+            data: articles,
+            statusCode: 200
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Erreur serveur',
+            status: 'NOT OK',
+            error: error.message,
+            statusCode: 500
+        });
+    }
+};
+
+exports.uneEconomie = async (req, res) => {
+    try {
+        const articles = await articleModel.find({categorie: "669b0b7cfe714afd6cf5738d"  , typeUne:  "rubrique" })
+            .sort({ date: -1 })  // Trier par date (du plus récent au plus ancien)
+            .limit(1)                  // Garder uniquement les 5 derniers
+            .populate(objectPopulate)
+            .exec();
+
+        return res.status(200).json({
+            message: 'Liste des 5 derniers articles récupérée avec succès',
+            status: 'OK',
+            data: articles[0],
+            statusCode: 200
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Erreur serveur',
+            status: 'NOT OK',
+            error: error.message,
+            statusCode: 500
+        });
+    }
+};
+
+exports.articleInvestigation = async (req, res) => {
+    try {
+        const articles = await articleModel.find({categorie: "66a1513888d3a2b60cc75d18"  , typeUne: { $not: /rubrique/ }})
+            .sort({ date: -1 })  // Trier par date (du plus récent au plus ancien)
+            .limit(5)                  // Garder uniquement les 5 derniers
+            .populate(objectPopulate)
+            .exec();
+
+        return res.status(200).json({
+            message: 'Liste des 5 derniers articles récupérée avec succès',
+            status: 'OK',
+            data: articles,
+            statusCode: 200
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Erreur serveur',
+            status: 'NOT OK',
+            error: error.message,
+            statusCode: 500
+        });
+    }
+};
+
+exports.uneInvestigation = async (req, res) => {
+    try {
+        const articles = await articleModel.find({categorie: "66a1513888d3a2b60cc75d18"  , typeUne:  "rubrique" })
+            .sort({ date: -1 })  // Trier par date (du plus récent au plus ancien)
+            .limit(1)                  // Garder uniquement les 5 derniers
+            .populate(objectPopulate)
+            .exec();
+
+        return res.status(200).json({
+            message: 'Liste des 5 derniers articles récupérée avec succès',
+            status: 'OK',
+            data: articles[0],
+            statusCode: 200
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Erreur serveur',
+            status: 'NOT OK',
+            error: error.message,
+            statusCode: 500
+        });
+    }
+};
+
+
+exports.articleContribution = async (req, res) => {
+    try {
+        const articles = await articleModel.find({categorie: "66a1525888d3a2b60cc75d1b"  , typeUne: { $not: /rubrique/ }})
+            .sort({ date: -1 })  // Trier par date (du plus récent au plus ancien)
+            .limit(7)                  // Garder uniquement les 5 derniers
+            .populate(objectPopulate)
+            .exec();
+
+        return res.status(200).json({
+            message: 'Liste des 5 derniers articles récupérée avec succès',
+            status: 'OK',
+            data: articles,
+            statusCode: 200
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Erreur serveur',
+            status: 'NOT OK',
+            error: error.message,
+            statusCode: 500
+        });
+    }
+};
+
+exports.articleChoixRedac = async (req, res) => {
+    try {
+        const articles = await articleModel.find({categorie: "66a1513888d3a2b60cc75d18"  , typeUne: { $not: /rubrique/ }})
+            .sort({ date: -1 })  // Trier par date (du plus récent au plus ancien)
+            .limit(4)                  // Garder uniquement les 5 derniers
+            .populate(objectPopulate)
+            .exec();
+
+        return res.status(200).json({
+            message: 'Liste des 5 derniers articles récupérée avec succès',
+            status: 'OK',
+            data: articles,
+            statusCode: 200
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Erreur serveur',
+            status: 'NOT OK',
+            error: error.message,
+            statusCode: 500
+        });
+    }
+};
+
+exports.articleSport = async (req, res) => {
+    try {
+        const articles = await articleModel.find({categorie: "669b0b86fe714afd6cf5738f"  , typeUne: { $not: /rubrique/ }})
+            .sort({ date: -1 })  // Trier par date (du plus récent au plus ancien)
+            .limit(5)                  // Garder uniquement les 5 derniers
+            .populate(objectPopulate)
+            .exec();
+
+        return res.status(200).json({
+            message: 'Liste des 5 derniers articles récupérée avec succès',
+            status: 'OK',
+            data: articles,
+            statusCode: 200
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Erreur serveur',
+            status: 'NOT OK',
+            error: error.message,
+            statusCode: 500
+        });
+    }
+};
+
+exports.articleCulture = async (req, res) => {
+    try {
+        const articles = await articleModel.find({categorie: "66a4810c87d32bedea8fa364"  , typeUne: { $not: /rubrique/ }})
+            .sort({ date: -1 })  // Trier par date (du plus récent au plus ancien)
+            .limit(5)                  // Garder uniquement les 5 derniers
+            .populate(objectPopulate)
+            .exec();
+
+        return res.status(200).json({
+            message: 'Liste des 5 derniers articles récupérée avec succès',
+            status: 'OK',
+            data: articles,
+            statusCode: 200
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Erreur serveur',
+            status: 'NOT OK',
+            error: error.message,
+            statusCode: 500
+        });
+    }
+};
+
+
+exports.articleAfrique = async (req, res) => {
+    try {
+        const articles = await articleModel.find({categorie: "669b0b98fe714afd6cf57391"  , typeUne: { $not: /rubrique/ }})
+            .sort({ date: -1 })  // Trier par date (du plus récent au plus ancien)
+            .limit(5)                  // Garder uniquement les 5 derniers
+            .populate(objectPopulate)
+            .exec();
+
+        return res.status(200).json({
+            message: 'Liste des 5 derniers articles récupérée avec succès',
+            status: 'OK',
+            data: articles,
+            statusCode: 200
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Erreur serveur',
+            status: 'NOT OK',
+            error: error.message,
+            statusCode: 500
+        });
+    }
+};
+
+exports.articleInternal = async (req, res) => {
+    try {
+        const articles = await articleModel.find({categorie: "669d23ad2d40ea6ee75cfc26"  , typeUne: { $not: /rubrique/ }})
+            .sort({ date: -1 })  // Trier par date (du plus récent au plus ancien)
+            .limit(5)                  // Garder uniquement les 5 derniers
+            .populate(objectPopulate)
+            .exec();
+
+        return res.status(200).json({
+            message: 'Liste des 5 derniers articles récupérée avec succès',
+            status: 'OK',
+            data: articles,
+            statusCode: 200
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Erreur serveur',
+            status: 'NOT OK',
+            error: error.message,
+            statusCode: 500
+        });
+    }
+};
+
 exports.slug = async (req, res) => {
 
     try {
--- routes/articles.js
+++ routes/articles.js
@@ -4,6 +4,19 @@ const router = new require('express').Router();
 router.get('/', require('../controllers/article').all);
 router.get('/top', require('../controllers/article').topArticle);
 router.get('/une', require('../controllers/article').uneArticles);
+router.get('/articleActualite', require('../controllers/article').articleActualite);
+router.get('/articlePolitique', require('../controllers/article').articlePolitique);
+router.get('/unePolitique', require('../controllers/article').unePolitique);
+router.get('/articleEconomie', require('../controllers/article').articleEconomie);
+router.get('/uneEconomie', require('../controllers/article').uneEconomie);
+router.get('/articleInvestigation', require('../controllers/article').articleInvestigation);
+router.get('/uneInvestigation', require('../controllers/article').uneInvestigation);
+router.get('/articleContribution', require('../controllers/article').articleContribution);
+router.get('/articleChoixRedac', require('../controllers/article').articleChoixRedac);
+router.get('/articleSport', require('../controllers/article').articleSport);
+router.get('/articleCulture', require('../controllers/article').articleCulture);
+router.get('/articleAfrique', require('../controllers/article').articleAfrique);
+router.get('/articleInternal', require('../controllers/article').articleInternal);
 router.get('/article/:slug', require('../controllers/article').slug);
 
 
